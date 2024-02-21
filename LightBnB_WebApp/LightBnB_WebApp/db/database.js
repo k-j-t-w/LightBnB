e63fa084da552abdf7458c166edfa9959ab414b9
Kai Wingfield <kaiwingfield17@gmail.com>
2024-02-21 11:44:01 -0800

    add db/index.js and refactor interactions with database to go through db/index.js

--- LightBnB_WebApp/LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/LightBnB_WebApp/db/database.js
@@ -1,14 +1,7 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'labber',
-  password: 'labber',
-  host: 'localhost',
-  database: 'lightbnb'
-});
+const db = require('./index.js');
 
 /// Users
 
@@ -18,18 +11,17 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  return pool
+  return db
     .query(`SELECT * FROM users WHERE email = $1`, [email])
     .then((result) => {
       if (result.rows.length > 0) {
-        console.log(result.rows[0]);
         return result.rows[0]; // Return the first user
       } else {
         return null; // No user found
       }
     })
     .catch((err) => { 
-      console.log(err.message, "--- This is the error message when running getUserWithEmail");
+      console.log(err.message);
     });
 };
 
@@ -39,18 +31,17 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return pool
+  return db
     .query(`SELECT * FROM users WHERE id = $1`, [id])
     .then((result) => {
       if (result.rows.length > 0) {
-        console.log(result.rows[0]);
         return result.rows[0];
       } else {
         return null;
       }
     })
     .catch((err) => {
-      console.log(err.message, "--- This is the error message when calling getUserWithId");
+      console.log(err.message);
     });
 };
 
@@ -60,7 +51,7 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  return pool
+  return db
     .query(`INSERT INTO users (name, email, password)
             VALUES ($1, $2, $3)
             RETURNING *`, [user.name, user.email, user.password])
@@ -68,7 +59,7 @@ const addUser = function (user) {
       return(result);
     })
     .catch((err) => {
-      console.log(err.message, "--- This is the error message when calling addUser")
+      console.log(err.message)
     })
 };
 
@@ -80,7 +71,7 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool
+  return db
     .query(`SELECT reservations.id, properties.*, reservations.start_date, reservations.end_date, reservations.guest_id, AVG(property_reviews.rating) AS average_rating
             FROM properties
             JOIN reservations ON properties.id = property_id
@@ -169,11 +160,7 @@ const getAllProperties = function (options, limit = 10) {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};`;
 
-  // 5
-  console.log(queryString, queryParams);
-
-  // 6
-  return pool.query(queryString, queryParams).then((res) => res.rows).catch((err) => console.log(err.message));
+  return db.query(queryString, queryParams).then((res) => res.rows).catch((err) => console.log(err.message));
 };
 
 /**
@@ -183,7 +170,7 @@ const getAllProperties = function (options, limit = 10) {
  */
 const addProperty = function (property) {
   console.log("Adding property:", property);
-  return pool
+  return db
     .query(`
     INSERT INTO properties (
       owner_id, 
@@ -218,7 +205,6 @@ const addProperty = function (property) {
       property.number_of_bedrooms
     ])
     .then((result) => {
-      console.log("Property added:", result.rows[0]);
       return(result.rows[0]);
     })
     .catch((err) => {
